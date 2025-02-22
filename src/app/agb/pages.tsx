960e5a5107478caa537babc411655ce251ef9150
Berliner108 <asdfasdfas@gmx.at>
2025-02-22 16:26:35 +0100

    Neues AGB-Update

--- src/app/agb/pages.tsx
+++ src/app/agb/pages.tsx
@@ -7,18 +7,6 @@ const AgbPage = () => {
   return (
     <main className={styles.container}>
       <h1 className={styles.heading}>Allgemeine Geschäftsbedingungen (AGB)</h1>
-      <h2 className={styles.subheading}>1. Allgemeines</h2>
-
-      <section className={styles.section}>
-        <p>Der Auftragnehmer (AN) erbringt für den Auftraggeber (AG) Dienstleistungen in der
-          Oberflächentechnik und im Bereich des Vertriebs von Arbeitsmitteln unter Einhaltung des beiliegenden, einen integrierenden
-          Bestandteil bildenden Service Level Agreements (SLAs).
-          Diese Allgemeinen Geschäftsbedingungen (AGB) gelten für alle gegenwärtigen und
-          zukünftigen Dienstleistungen, die der AN gegenüber dem AG erbringt, auch wenn
-          im Einzelfall bei Vertragsabschluss nicht ausdrücklich auf die AGB Bezug
-          genommen wird. Geschäftsbedingungen des AG gelten nur, wenn sie vom AN
-          schriftlich anerkannt wurden.</p>
-      </section>
       <section className={styles.section}>
         <p>Der Auftragnehmer (AN) erbringt für den Auftraggeber (AG) Dienstleistungen in der
           Oberflächentechnik und im Bereich des Vertriebs von Arbeitsmitteln unter Einhaltung des beiliegenden, einen integrierenden
