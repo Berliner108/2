9ebfb9a56ac51f85f82ed33972a72fe8619cbe52
Berliner108 <asdfasdfas@gmx.at>
2025-02-22 17:05:14 +0100

    Neues AGB-Update

--- src/app/agb/page.tsx
+++ src/app/agb/page.tsx
@@ -19,19 +19,16 @@ const AgbPage = () => {
           genommen wird. Geschäftsbedingungen des AG gelten nur, wenn sie vom AN
           schriftlich anerkannt wurden.</p>
       </section>
-      <h2 className={styles.subheading}>1. Allgemeines</h2>
+      <h2 className={styles.subheading}>2. Geltungsbereich</h2>
 
       <section className={styles.section}>
-        <p>Der Auftragnehmer (AN) erbringt für den Auftraggeber (AG) Dienstleistungen in der
-          Oberflächentechnik und im Bereich des Vertriebs von Arbeitsmitteln unter Einhaltung des beiliegenden, einen integrierenden
-          Bestandteil bildenden Service Level Agreements (SLAs).
-          Diese Allgemeinen Geschäftsbedingungen (AGB) gelten für alle gegenwärtigen und
-          zukünftigen Dienstleistungen, die der AN gegenüber dem AG erbringt, auch wenn
-          im Einzelfall bei Vertragsabschluss nicht ausdrücklich auf die AGB Bezug
-          genommen wird. Geschäftsbedingungen des AG gelten nur, wenn sie vom AN
-          schriftlich anerkannt wurden.</p>
+        <p>Die AGB gelten für alle Verträge, die über die Plattform geschlossen werden. Abweichende Bedingungen werden nicht anerkannt, es sei denn, sie wurden ausdrücklich schriftlich bestätigt. Änderungen oder Ergänzungen der AGB werden den Nutzern mitgeteilt und treten mit der nächsten Nutzung der Plattform in Kraft.
+        Diese Allgemeinen Geschäftsbedingungen (AGB) regeln die Nutzung der über die Website www.beschichterscout.de zugänglichen Services der Beschichter Scout GmbH durch Privatkunden. Sie gelten für sämtliche Rechtsverhältnisse zwischen der Beschichter Scout GmbH und den privaten Nutzern der Fahrzeug-Datenbank.
+        </p>
+        <p>Die AGB gelten für alle Verträge, die über die Plattform geschlossen werden. Abweichende Bedingungen werden nicht anerkannt, es sei denn, sie wurden ausdrücklich schriftlich bestätigt. Änderungen oder Ergänzungen der AGB werden den Nutzern mitgeteilt und treten mit der nächsten Nutzung der Plattform in Kraft.
+        </p>
       </section>
-      <h2 className={styles.subheading}>1. Allgemeines</h2>
+      <h2 className={styles.subheading}>3. Leistungen von Beschichter Scout</h2>
 
       <section className={styles.section}>
         <p>Der Auftragnehmer (AN) erbringt für den Auftraggeber (AG) Dienstleistungen in der
