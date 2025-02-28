32c765bac94fc323e9567a29dcb157849ae72a6f
Berliner108 <asdfasdfas@gmx.at>
2025-02-28 16:34:55 +0100

    as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Page() {
       <nav className={styles.navbar}>
         <ul className={styles.navList}>
           {[
-            { title: 'Angebote einholen', href: '/angebote', links: [{ href: '/about/team', text: 'Lackieren' },  { href: '/about/vision', text: 'Pulverbeschichten' }] },
-            { title: 'Kaufen', href: '/kaufen', links: [{ href: '/about/team', text: 'Unser Team' }, { href: '/about/vision', text: 'Unsere Vision' }, { href: '/about/vision', text: 'Unsere Vision' }, { href: '/about/vision', text: 'Unsere Vision' }, { href: '/about/vision', text: 'Unsere Vision' }] },
-            { title: 'Lacke anfragen', href: '/sonderlacke', links: [{ href: '/services/webdesign', text: 'Webdesign' }, { href: '/services/seo', text: 'SEO' }] },
-            { title: 'Auftragsbörse', href: '/auftragsboerse', links: [{ href: '/contact/email', text: 'E-Mail' }, { href: '/contact/phone', text: 'Telefon' }] },
-            { title: 'Verkaufen', href: '/verkaufen', links: [{ href: '/portfolio/websites', text: 'Websites' }, { href: '/portfolio/apps', text: 'Apps' }] },
-            { title: 'Offene Lackanfragen', href: '/lackanfragen', links: [{ href: '/blog/latest', text: 'Neueste Artikel' }, { href: '/blog/popular', text: 'Beliebteste Artikel' }] },
+            { title: 'Angebote einholen', href: '/angebote', links: [{ href: '/about/team', text: 'Lackieren' },  { href: '/about/vision', text: 'Pulverbeschichten' },  { href: '/about/vision', text: 'Verzinken' },  { href: '/about/vision', text: 'Eloxieren' },  { href: '/about/vision', text: 'Strahlen' },  { href: '/about/vision', text: 'Entlacken' },  { href: '/about/vision', text: 'Einlagern' },  { href: '/about/vision', text: 'Isolierstegverpressung' },  { href: '/about/vision', text: 'Folieren' },  { href: '/about/vision', text: 'Kominiert' }] },
+            { title: 'Kaufen', href: '/kaufen', links: [{ href: '/about/team', text: 'Nasslacke' }, { href: '/about/vision', text: 'Pulverlacke' }, { href: '/about/vision', text: 'Arbeitsmittel' }] },
+            { title: 'Lacke anfragen', href: '/sonderlacke', links: [{ href: '/services/webdesign', text: 'Sonderfarbe Nasslack' }, { href: '/services/seo', text: 'Sonderfarbe Pulverlack' }] },
+            { title: 'Auftragsbörse', href: '/auftragsboerse', links: [{ href: '/about/team', text: 'Lackieren' },  { href: '/about/vision', text: 'Pulverbeschichten' },  { href: '/about/vision', text: 'Verzinken' },  { href: '/about/vision', text: 'Eloxieren' },  { href: '/about/vision', text: 'Strahlen' },  { href: '/about/vision', text: 'Entlacken' },  { href: '/about/vision', text: 'Einlagern' },  { href: '/about/vision', text: 'Isolierstegverpressung' },  { href: '/about/vision', text: 'Folieren' },  { href: '/about/vision', text: 'Kominiert' }] },
+            { title: 'Verkaufen', href: '/verkaufen', links: [{ href: '/about/team', text: 'Nasslacke' }, { href: '/about/vision', text: 'Pulverlacke' }, { href: '/about/vision', text: 'Arbeitsmittel' }] },
+            { title: 'Offene Lackanfragen', href: '/lackanfragen', links: [{ href: '/services/webdesign', text: 'Sonderfarbe Nasslack' }, { href: '/services/seo', text: 'Sonderfarbe Pulverlack' }] },
             { title: 'Wissenswertes', href: '/wissenswertes', links: [{ href: '/faq/shipping', text: 'Versand' }, { href: '/faq/payment', text: 'Zahlung' }] },
             { title: 'Mein Konto', href: '/konto', links: [{ href: '/support/help', text: 'Hilfe' }, { href: '/support/contact', text: 'Kontakt' }] }
           ].map((item, index) => (
