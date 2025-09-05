e56cdaaeb7e0152a0f564f61afa8dbd1303fb625
Berliner108 <asdfasdfas@gmx.at>
2025-09-05 19:15:30 +0200

    prerof

--- src/app/konto/orders/[id]/page.tsx
+++ src/app/konto/orders/[id]/page.tsx
@@ -5,7 +5,7 @@ import OrderActions from '../OrderActions'
 export const dynamic = 'force-dynamic'
 
 export default async function OrderDetailPage(
-  { params }: PageProps<'/konto/orders/[id]'>
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params
 
