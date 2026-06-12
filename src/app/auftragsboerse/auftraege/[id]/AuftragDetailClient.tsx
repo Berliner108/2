60c247852218965033717f9a2ca21dfb0992cb07
Berliner108 <asdfasdfas@gmx.at>
2026-06-12 14:13:23 +0200

    frg

--- src/app/auftragsboerse/auftraege/[id]/AuftragDetailClient.tsx
+++ src/app/auftragsboerse/auftraege/[id]/AuftragDetailClient.tsx
@@ -914,17 +914,17 @@ const serienTermine = Array.isArray((auftrag as any).serien_termine)
         </div>
       )}
       {(gesamtPreis.trim() || logistikPreis.trim()) && (
-  <div className={styles.priceTotal}>
-    Gesamt:{' '}
-    {(
-      (parseMoneyOrNull(gesamtPreis) ?? 0) +
-      (brauchtLogistikPreis ? parseMoneyOrNull(logistikPreis) ?? 0 : 0)
-    ).toLocaleString('de-DE', {
-      style: 'currency',
-      currency: 'EUR',
-    })}
-  </div>
-)}
+        <div className={styles.priceTotal}>
+        Gesamt:{' '}
+        {(
+          (parseMoneyOrNull(gesamtPreis) ?? 0) +
+          (brauchtLogistikPreis ? parseMoneyOrNull(logistikPreis) ?? 0 : 0)
+        ).toLocaleString('de-DE', {
+          style: 'currency',
+          currency: 'EUR',
+        })}
+      </div>
+        )}
 
       <button type="submit" className={styles.buyButton} disabled={isSubmitDisabled}>
         {offerSent ? 'Angebot erfolgreich abgegeben' : loading ? 'Sende…' : 'Angebot abgeben'}
