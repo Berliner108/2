b0a4c07b57d05221cd1d28bf1dc92ca036de35d3
Berliner108 <asdfasdfas@gmx.at>
2026-06-29 10:08:35 +0200

    4grtbg

--- src/app/auftragsboerse/loading.tsx
+++ src/app/auftragsboerse/loading.tsx
@@ -1,5 +1,20 @@
-import BoerseLoading from '../components/loading/BoerseLoading'
+'use client';
+
+import { usePathname } from 'next/navigation';
+import BoerseLoading from '../components/loading/BoerseLoading';
+
+function shouldHideBoerseLoading(pathname: string | null) {
+  if (!pathname) return false;
+
+  return /^\/auftragsboerse\/auftraege\/[^/]+\/?$/.test(pathname);
+}
 
 export default function Loading() {
-  return <BoerseLoading />
+  const pathname = usePathname();
+
+  if (shouldHideBoerseLoading(pathname)) {
+    return null;
+  }
+
+  return <BoerseLoading />;
 }
